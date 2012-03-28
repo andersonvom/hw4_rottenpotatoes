c27b308eac658f4b5624833fa28c24a1da60346d
Anderson Mesquita <andersonvom@gmail.com>
2012-03-27 21:45:03 -0400

    Added test to verify if 'similar' action is searching for movies from the same director

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe MoviesController do
 
-  it "GET 'similar'" do
-    movie = mock_model('Movie')
-    get :similar, :id => movie.id
-    response.should be_success
+  context "'similar' action" do
+
+    before :each do
+      @movie = mock_model('Movie', :director => 'George Lucas')
+    end
+
+    it "GET 'similar'" do
+      get :similar, :id => @movie.id
+      response.should render_template('similar')
+    end
+    
+    it "should find all movies from the director of given movie" do
+      get :similar, :id => @movie.id
+      Movie.should_receive(:find_by_director).with('George Lucas')
+    end
+  
   end
 
 end
