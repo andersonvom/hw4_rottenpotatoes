ef576d7a7e209d6ebe30e4362128a7b5361d4ef9
Anderson Mesquita <andersonvom@gmail.com>
2012-03-27 22:23:25 -0400

    Removed unused code in movie routes spec

--- spec/routing/movie_routes_spec.rb
+++ spec/routing/movie_routes_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe 'Routing for Movie' do
 
-  before :each do
-    @movie = Movie.create :title => 'Temp'
-  end
-
   it "should have a named route to 'similar movies'" do
     { :get => 'movies/3/similar' }.
       should route_to(
