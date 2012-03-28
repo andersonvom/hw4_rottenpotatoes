bab2a144c3b3b6736d34c0eb6270af776652e01a
Anderson Mesquita <andersonvom@gmail.com>
2012-03-27 20:29:26 -0400

    Added step: 'the director of "Movie" should be "Director"'

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -5,4 +5,7 @@ Given /^the following movies exist:$/ do |table|
   end
 end
 
-
+Then /the director of "([^"]+)" should be "([^"]+)"/ do |movie_name, director_name|
+  movie = Movie.find_by_title movie_name
+  movie.director.should == director_name
+end
