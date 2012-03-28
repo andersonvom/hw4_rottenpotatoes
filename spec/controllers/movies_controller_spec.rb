0ed38d897d0a164742d958f2ec8ede638a5efc51
Anderson Mesquita <andersonvom@gmail.com>
2012-03-27 22:03:17 -0400

    Refactored MoviesController tests and added check for find_by_director to 'similar' action

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -6,16 +6,21 @@ describe MoviesController do
 
     before :each do
       @movie = mock_model('Movie', :director => 'George Lucas')
+      @fake_results = [mock_model('Movie'), mock_model('Movie')]
+      Movie.stub(:find).and_return @movie
     end
 
     it "GET 'similar'" do
+      Movie.stub(:find_by_director)
       get :similar, :id => @movie.id
       response.should render_template('similar')
     end
     
     it "should find all movies from the director of given movie" do
+      Movie.should_receive(:find_by_director).with('George Lucas').
+        and_return(@fake_results)
       get :similar, :id => @movie.id
-      Movie.should_receive(:find_by_director).with('George Lucas')
+      assigns(:movies).should == @fake_results
     end
   
   end
